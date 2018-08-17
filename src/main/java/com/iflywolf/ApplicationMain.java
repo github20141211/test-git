f0f3264b07b092ed728449086aaed0dc23f44284
luocheng <444790866@qq.com>
2018-08-17 09:43:07 +0800

    hox_fix

--- src/main/java/com/iflywolf/ApplicationMain.java
+++ src/main/java/com/iflywolf/ApplicationMain.java
@@ -5,6 +5,7 @@ public class ApplicationMain {
 	public static void main(String[] args) {
 		System.out.println("right git........");
 		System.out.println("left git........");
+		System.out.println("hox_fix git........");
 	}
 
 }
